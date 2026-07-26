00b1f12bae45d4b27e984f491b9eda55ead8d601
Rohit Prasad <73442980+rohitprasad-code@users.noreply.github.com>
2026-07-26 08:03:52 +0530

    Sync: Maximum Product of Three Numbers (cpp)

--- leetcode-submissions/Algorithms/Maximum Product of Three Numbers/MaximumProductofThreeNumbers.cpp
+++ leetcode-submissions/Algorithms/Maximum Product of Three Numbers/MaximumProductofThreeNumbers.cpp
@@ -1,29 +1,21 @@
 /*
-  LeetCode Submission ID: 2081338852
-  Date: 26/07/2026, 08:02:58
-  Runtime: 0 ms
+  LeetCode Submission ID: 2081340055
+  Date: 26/07/2026, 08:03:51
+  Runtime: 7 ms
   Memory: 31.5 MB
   Difficulty: Easy
-  Problem Link: https://leetcode.com/problems/maximum-product-of-three-numbers/submissions/2081338852/?envType=daily-question&envId=2026-07-26
+  Problem Link: https://leetcode.com/problems/maximum-product-of-three-numbers/submissions/2081340055/?envType=daily-question&envId=2026-07-26
 */
 
 class Solution {
 public:
     int maximumProduct(vector<int>& A) {
-        int a = -1001, b = a, c = b;    // max 3
-        int x =  1001, y = x;           // min 2
-
-        for (auto& n : A) {
-            int pa = a, pb = b, px = x;
-
-            a = max(a, n);
-            b = max(b, min(pa, n));
-            c = max(c, min(pb, n));
-
-            x = min(x, n);
-            y = min(y, max(px, n));
-        }
-
-        return max(a * b * c, a * x * y);
+        ranges::sort(A);
+        int n = A.size();
+        
+        return max(
+            A.back() * A[n - 2] * A[n - 3],
+            A.back() * A.front() * A[1]
+        );
     }
 };
