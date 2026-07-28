a3ca077ee1088431ef872d8ae03923f31f7a2bab
Rohit Prasad <73442980+rohitprasad-code@users.noreply.github.com>
2026-07-29 04:16:13 +0530

    Sync: Q1. Set Mismatch (cpp)

---
+++ leetcode-submissions/Algorithms/Q1. Set Mismatch/Q1SetMismatch.cpp
@@ -0,0 +1,32 @@
+/*
+  LeetCode Submission ID: 2085386247
+  Date: 29/07/2026, 04:16:11
+  Runtime: 0 ms
+  Memory: 26.4 MB
+  Difficulty: Medium
+  Problem Link: https://leetcode.com/problems/set-mismatch/submissions/2085386247/?envType=problem-list-v2&envId=dsa-linear-shoal-array-ii
+*/
+
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> freq(n + 1, 0);
+
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        int duplicate = -1;
+        int missing = -1;
+
+        for (int i = 1; i <= n; i++) {
+            if (freq[i] == 2)
+                duplicate = i;
+            else if (freq[i] == 0)
+                missing = i;
+        }
+
+        return {duplicate, missing};
+    }
+};
