2be9515b45bace872b78f7b36b57da90bb244135
Rohit Prasad <73442980+rohitprasad-code@users.noreply.github.com>
2026-08-02 20:40:45 +0530

    Sync: Stone Game (cpp)

---
+++ leetcode-submissions/Algorithms/Stone Game/StoneGame.cpp
@@ -0,0 +1,15 @@
+/*
+  LeetCode Submission ID: 2091520241
+  Date: 02/08/2026, 20:40:44
+  Runtime: 0 ms
+  Memory: 10.5 MB
+  Difficulty: Easy
+  Problem Link: https://leetcode.com/problems/stone-game/submissions/2091520241/?envType=daily-question&envId=2026-08-02
+*/
+
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
