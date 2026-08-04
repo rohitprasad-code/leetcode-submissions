7a90bfb3a8b27c42ce915e5ffab516094bd08e09
Rohit Prasad <73442980+rohitprasad-code@users.noreply.github.com>
2026-08-04 05:57:35 +0530

    Sync: Stone Game III (cpp)

---
+++ leetcode-submissions/Algorithms/Stone Game III/StoneGameIII.cpp
@@ -0,0 +1,29 @@
+/*
+  LeetCode Submission ID: 2093274801
+  Date: 04/08/2026, 05:57:34
+  Runtime: 3 ms
+  Memory: 130.4 MB
+  Difficulty: Easy
+  Problem Link: https://leetcode.com/problems/stone-game-iii/submissions/2093274801/?envType=daily-question&envId=2026-08-03
+*/
+
+class Solution {
+public:
+    static inline string s[] = {"Bob", "Tie", "Alice"};
+    string stoneGameIII(vector<int>& A) {
+        int n = A.size();
+        int dp[4] = {0, 0, 0, 0};
+
+        for (int i = n - 1; i >= 0; i--) {
+            int j = i & 3;
+
+            dp[j] = A[i] - dp[(i + 1) & 3];
+            if (i + 2 <= n)
+                dp[j] = max(dp[j], A[i] + A[i + 1] - dp[(i + 2) & 3]);
+            if (i + 3 <= n)
+                dp[j] = max(dp[j], A[i] + A[i + 1] + A[i + 2] - dp[(i + 3) & 3]);
+        }
+
+        return s[(dp[0] > 0) - (dp[0] < 0) + 1];
+    }
+};
