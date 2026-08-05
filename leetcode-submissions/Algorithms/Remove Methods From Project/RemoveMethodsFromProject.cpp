4d6e392e684fd67b90f062caf1e95f0744efc52e
Rohit Prasad <73442980+rohitprasad-code@users.noreply.github.com>
2026-08-06 02:57:42 +0530

    Sync: Remove Methods From Project (cpp)

---
+++ leetcode-submissions/Algorithms/Remove Methods From Project/RemoveMethodsFromProject.cpp
@@ -0,0 +1,64 @@
+/*
+  LeetCode Submission ID: 2095908494
+  Date: 06/08/2026, 02:57:41
+  Runtime: 165 ms
+  Memory: 302.3 MB
+  Difficulty: Easy
+  Problem Link: https://leetcode.com/problems/remove-methods-from-project/submissions/2095908494/?envType=daily-question&envId=2026-08-05
+*/
+
+constexpr int MAXN = 100005;
+
+class Solution {
+public:
+    vector<int> remainingMethods(int n, int k,
+                                 vector<vector<int>>& invocations) {
+        vector<vector<int>> edges(n);
+        vector<int> inDegree(n, 0);
+
+        bitset<MAXN> suspicious;
+
+        for (const auto& inv : invocations) {
+            edges[inv[0]].push_back(inv[1]);
+            inDegree[inv[1]]++;
+        }
+
+        queue<int> q;
+        q.push(k);
+
+        suspicious.set(k);
+
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            for (int v : edges[u]) {
+                inDegree[v]--;
+
+                if (!suspicious.test(v)) {
+                    q.push(v);
+                    suspicious.set(v);
+                }
+            }
+        }
+
+        bool canRemoveAll = true;
+        vector<int> remaining;
+
+        for (int i = 0; i < n; i++) {
+            if (suspicious.test(i) && inDegree[i] > 0) {
+                canRemoveAll = false;
+                break;
+            } else if (!suspicious.test(i)) {
+                remaining.push_back(i);
+            }
+        }
+
+        if (!canRemoveAll) {
+            vector<int> allNodes(n);
+            iota(allNodes.begin(), allNodes.end(), 0);
+            return allNodes;
+        }
+
+        return remaining;
+    }
+};
